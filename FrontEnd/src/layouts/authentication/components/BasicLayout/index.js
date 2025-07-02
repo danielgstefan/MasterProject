6a62bce5bab4850bcfd77e491490d07a254a3474
danielgstefan <danycsov16@gmail.com>
2025-07-03 02:11:18 +0300

    v5.2 finished

--- FrontEnd/src/layouts/authentication/components/BasicLayout/index.js
+++ FrontEnd/src/layouts/authentication/components/BasicLayout/index.js
@@ -20,15 +20,6 @@ import Footer from "layouts/authentication/components/Footer";
 function BasicLayout({ title, description, image, children }) {
   return (
     <PageLayout>
-      <DefaultNavbar
-        action={{
-          type: "external",
-          route: "https://creative-tim.com/product/vision-ui-dashboard-react",
-          label: "Free Download",
-        }}
-        transparent
-        light
-      />
       <VuiBox
         width="calc(100% - 2rem)"
         minHeight="50vh"
